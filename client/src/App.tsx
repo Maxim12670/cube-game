ba50678ec171050e6251f509bb8137930bbdd358
Максим <akaikinmax@mail.ru>
2024-07-08 00:09:31 +0700

    feat/ базовая верстка главной страницы

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import './app.scss';
+import MainPage from './pages/MainPage/MainPage';
+
+// const selectValues = [1.00, 2.00, 3.00, 5.00, 10.00, 25.00, 60.00, 100.00];
 
 export default function App() {
 
   return (
-    <>
-      <h2 className='myTitle'>hello world!!!</h2>
-    </>
+    <MainPage />
   )
 }
