0b05143c6ac001d96de7f2dce89c184a712ba587
Максим <akaikinmax@mail.ru>
2024-07-13 16:05:16 +0700

    push

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/cube-game/",
   optimizeDeps: {
     exclude: ['js-big-decimal']
   },
